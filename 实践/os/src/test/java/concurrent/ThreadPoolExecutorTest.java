222a9338d1f1165e56fb892de833f577154be269
fofcn <errorfatal89@gmail.com>
2022-01-05 11:40:20 +0800

    1、增加ThreadPoolExecutor最大线程数量测试

--- 实践/os/src/test/java/concurrent/ThreadPoolExecutorTest.java
+++ 实践/os/src/test/java/concurrent/ThreadPoolExecutorTest.java
@@ -1,6 +1,7 @@
 package concurrent;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import util.StdOut;
 
@@ -14,6 +15,70 @@ import java.util.concurrent.atomic.AtomicInteger;
  * ThreadPoolExecutor测试类
  */
 public class ThreadPoolExecutorTest {
+    /**
+     * 线程池中线程索引
+     */
+    private final AtomicInteger pooledThreadIdx = new AtomicInteger(0);
+
+    /**
+     * 核心线程数
+     */
+    private final int coreSizeN = 1;
+
+    /**
+     * 最大线程数
+     */
+    private final int maxSizeN = 3;
+
+    /**
+     * 任务最大排队数量
+     */
+    private final int queueSizeM = 10;
+
+    /**
+     * 线程池中大于coreSize的线程空闲时间，单位：毫秒
+     */
+    private final long keepAliveTime = 60L * 1000;
+
+    /**
+     * 线程池
+     */
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    /**
+     * 控制线程池任务执行开关
+     */
+    private final AtomicBoolean pass = new AtomicBoolean(false);
+
+    @Before
+    public void before() {
+        // 新建线程池
+        threadPoolExecutor = new ThreadPoolExecutor(
+                coreSizeN,
+                maxSizeN,
+                keepAliveTime,
+                TimeUnit.MILLISECONDS,
+                // 任务队列为最大排队为10的任务队列
+                new ArrayBlockingQueue<>(queueSizeM),
+                // 定制ThreadFactory，定义线程名称，以在多个线程池场景下区分业务线程
+                r -> new Thread(r, "executor-tester-" + pooledThreadIdx.getAndIncrement()),
+
+                // 如果排队数量超过10，且线程最大已经达到maximumPoolSize时，再有任务提交时的拒绝策略
+                // 一般是直接拒绝：表示服务仅能支撑这么多
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+    }
+
+    /**
+     * 测试线程池线程数量等于最大线程数用例
+     * @throws InterruptedException
+     */
+    @Test
+    public void testThreadCountGreaterThanCoreSize() throws InterruptedException {
+        // 向线程池提交n + m个任务
+        submitTask(threadPoolExecutor, pass, maxSizeN + queueSizeM);
+        startControlThread(pass, maxSizeN, threadPoolExecutor);
+    }
 
     @Test
     public void testThreadCountNotGreaterThanCoreSize() throws InterruptedException {
@@ -42,6 +107,47 @@ public class ThreadPoolExecutorTest {
         submitTask(threadPoolExecutor, pass, 11);
 
         // 控制线程
+        startControlThread(pass, 1, threadPoolExecutor);
+    }
+
+    /**
+     * 提交任务到线程池
+     * @param threadPoolExecutor 线程池
+     * @param pass 任务控制开关
+     * @param taskCount 任务数量
+     */
+    private void submitTask(ThreadPoolExecutor threadPoolExecutor, AtomicBoolean pass, int taskCount) {
+        for (int i = 0; i < taskCount; i++) {
+            threadPoolExecutor.submit(() -> {
+                while (!pass.get()) {
+                    StdOut.println(Thread.currentThread().getName() + ": Thread running..." );
+                    sleep(1000);
+                }
+            });
+        }
+    }
+
+    /**
+     * 睡眠
+     * @param millis 睡眠时间，单位：毫秒
+     */
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 控制线程池中任务执行的线程
+     * @param pass 任务执行开关 true:任务执行，false：任务睡眠
+     * @param maxSizeN  线程池中最大线程数量
+     * @param threadPoolExecutor 线程池
+     * @throws InterruptedException
+     */
+    private void startControlThread(AtomicBoolean pass, int maxSizeN, ThreadPoolExecutor threadPoolExecutor) throws InterruptedException {
+        // 控制线程
         Thread controlThread = new Thread(() -> {
             int i = 0;
             while (i++ < 10) {
@@ -49,7 +155,7 @@ public class ThreadPoolExecutorTest {
                 sleep(1000);
 
                 // 睡眠一秒后再获取活动的线程数量应该为1,
-                Assert.assertEquals(1, threadPoolExecutor.getActiveCount());
+                Assert.assertEquals(maxSizeN, threadPoolExecutor.getActiveCount());
                 StdOut.println("thread pool running workers: " + threadPoolExecutor.getActiveCount());
             }
 
@@ -70,22 +176,4 @@ public class ThreadPoolExecutorTest {
         controlThread.join();
     }
 
-    private void submitTask(ThreadPoolExecutor threadPoolExecutor, AtomicBoolean pass, int taskCount) {
-        for (int i = 0; i < taskCount; i++) {
-            threadPoolExecutor.submit(() -> {
-                while (!pass.get()) {
-                    StdOut.println(Thread.currentThread().getName() + ": Thread running..." );
-                    sleep(1000);
-                }
-            });
-        }
-    }
-
-    public static void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
